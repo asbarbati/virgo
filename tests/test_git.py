fb509757807c6a89efa620d6b2c21302b88bdf4d
Alessandro Sbarbati <122787746+asbarbati@users.noreply.github.com>
2024-12-12 22:15:36 +0100

    test: fix test detection on gh actions

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,5 +1,6 @@
 import structlog
 from uptainer.git import Git
+from os import getenv
 
 log = structlog.get_logger()
 
@@ -9,17 +10,18 @@ def test_git_createworkdir():
     assert check.startswith("/tmp")
 
 def test_git_clone_repo():
-    git_obj = Git(log=log, remote_url="git@github.com:Mirio/verbacap.git", branch="nonexists", ssh_private_key="id_rsa")
+    homedir = getenv("HOME", "/home/runner")
+    git_obj = Git(log=log, remote_url="git@github.com:Mirio/verbacap.git", branch="nonexists", ssh_private_key=f"${homedir}/.ssh/id_rsa")
     git_obj.create_workdir()
     check = git_obj.clone_repo()
     assert check["error"] == True
 
-    git_obj = Git(log=log, remote_url="git@github.com:Mirio/verbacap.git", branch="main", ssh_private_key="id_rsa")
+    git_obj = Git(log=log, remote_url="git@github.com:Mirio/verbacap.git", branch="main", ssh_private_key=f"${homedir}/.ssh/id_rsa")
     git_obj.create_workdir()
     check = git_obj.clone_repo()
     assert check["error"] == False
 
-    git_obj = Git(log=log, remote_url="https://github.com/Mirio/verbacap.git", branch="main", ssh_private_key="id_rsa")
+    git_obj = Git(log=log, remote_url="https://github.com/Mirio/verbacap.git", branch="main", ssh_private_key=f"${homedir}/.ssh/id_rsa")
     git_obj.create_workdir()
     check = git_obj.clone_repo()
     assert check["error"] == True
